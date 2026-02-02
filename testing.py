e2ef63fd73cc755579c5c1b02e149ae95b0edccd
DRC <drc@example.com>
2026-02-02 16:04:50 +0530

    added test

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@
 This module contains a simple hello function.
 """
 
+
 def hello():
     """
     This function returns a greeting message.
@@ -11,4 +12,3 @@ def hello():
 
 if __name__ == "__main__":
     hello()
-    
